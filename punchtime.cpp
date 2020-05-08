b68f76f31f88aafe37a1b885a3741fb67beaa918
Jero Bado <tokidokitalkyou@gmail.com>
2020-05-08 20:55:05 +0800

    Removed repeating codes by adding functions

--- punchtime.cpp
+++ punchtime.cpp
@@ -13,13 +13,6 @@ $ punctime 7:47 -b 11:00 -o 1   # time-in with breaktime and 1 hour overtime (-o
 Break until: 12:30
 Time-out: 17:47
 
-
-TODO: 
-1. get user input
-2. parse user input, get minutes and seconds
-3. add defualt 9 working hours
-4. print result
-
 */
 
 #include <iostream>
@@ -32,6 +25,17 @@ TODO:
 #include <cstring>
 
 using namespace std;
+
+
+// Declaring global variables
+int colon_pos;
+int hour_int;
+int min_int;
+int overtime_int = 0;
+string input;
+string hour_str;
+string min_str;
+bool isOvertime;
 
 
 void display_current_time(void) {
@@ -66,7 +70,9 @@ void display_current_time2(void) {
 
 }
 
-
+// Function declaration
+void parse_input(string input);
+void convert_input(void);
 
 
 int main(int argc, char **argv)
@@ -74,14 +80,8 @@ int main(int argc, char **argv)
     int index;
     int positional = 1;
     int opt;
-    int colon_pos;
-    int hour_int;
-    int min_int;
     int out_hour_int;
     int out_min_int;
-    string input;
-    string hour_str;
-    string min_str;
    
     // Parse optional arguments
     while ((opt = getopt(argc, argv, ":b:o:-v")) != -1)
@@ -93,13 +93,10 @@ int main(int argc, char **argv)
                 input = optarg;
                 
                 // Parse input, get hours and minutes
-                colon_pos = input.find(":");                            // find colon
-                hour_str = input.substr(0, colon_pos);                  // get hour
-                min_str = input.substr(colon_pos + 1, input.length());  // get minutes    
-
+                parse_input(input);
+               
                 // Convert hour and minutes to digits
-                hour_int = stoi(hour_str);
-                min_int = stoi(min_str);    
+                convert_input();
 
                 // Add default working hours (1 hour 30 minutes)
                 out_hour_int = hour_int + 1;
@@ -110,8 +107,11 @@ int main(int argc, char **argv)
                 break;
             
             case 'o':
-                cout << "optarg = " << optarg << endl;
-                cout << "[] TODO: compute overtime" << endl;
+                // Get user input
+                input = optarg;
+                
+                // Convert input to int
+                overtime_int = stoi(input);
                 break;
 
             case 'v':
@@ -137,20 +137,16 @@ int main(int argc, char **argv)
         input = argv[index];   
             
         // Parse input, get hours and minutes
-        colon_pos = input.find(":");                            // find colon
-        hour_str = input.substr(0, colon_pos);                  // get hour
-        min_str = input.substr(colon_pos + 1, input.length());  // get minutes    
+        parse_input(input);
 
         // Convert hour and minutes to digits
-        hour_int = stoi(hour_str);
-        min_int = stoi(min_str);    
+        convert_input();
 
-        // Add default working hours (9)
-        out_hour_int = hour_int + 9;
+        // Add default working hours (9) and overtime
+        out_hour_int = hour_int + 9 + overtime_int;
         out_min_int = min_int;
 
         // Display result
-        // cout << "Time-in: " << hour_int << ":" << min_int << endl;
         cout << "Time-out: " << out_hour_int << ":" << out_min_int << endl;
 
         // Exit for loop, we only need to process 1 positional non-argument
@@ -160,3 +156,22 @@ int main(int argc, char **argv)
     
     return 0;
 }
+
+// Function definition
+void parse_input(string input)
+{
+    /* Parse input getting hours and minutes */
+
+    colon_pos = input.find(":");                            // find colon
+    hour_str = input.substr(0, colon_pos);                  // get hour
+    min_str = input.substr(colon_pos + 1, input.length());  // get minutes    
+}
+
+void convert_input()
+{
+    /* Convert parsed hour and minutes from string to int */
+    
+    hour_int = stoi(hour_str);
+    min_int = stoi(min_str);
+}
+
