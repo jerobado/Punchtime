f07211d6f02dbb72a5625883d9cc4f130d44d828
Jero Bado <tokidokitalkyou@gmail.com>
2020-07-02 17:41:11 +0800

    Declared varabiales for in, out and breaktime
    Temporarily declared variables to hold time-in, time-out and breaktime. This will be implemented soon on a 'Time' class.

--- punchtime.h
+++ punchtime.h
@@ -4,8 +4,21 @@
 class Punchtime {
 
     public:
-        int hour = 0;
-        int minute = 0;
+        int in_hour = 0;
+        int in_minute = 0;
+
+        int out_hour = 0;
+        int out_minute = 0;
+
+        int break_hour = 0;
+        int break_minute = 0;
+
+        // [] TODO: create a Time class
+        // Usage:
+        // Time inTime(0, 0);
+        // Time outTime(0, 0);
+        // Time breakTime(0, 0);
+
         int workingHours = 9;
         void setTimeout(int, int);
         void timeout();
